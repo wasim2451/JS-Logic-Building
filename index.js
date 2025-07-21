909553b3201eb29637ee49dbfb8e9b8d7c577781
[Wasim Akhtar Khan] <wasimakhtarkhan2003@gmail.com>
2025-07-21 22:11:41 +0530

    Test 8 Completed✅

--- index.js
+++ index.js
@@ -25,8 +25,7 @@ function transpose(arr) {
     console.log("After Transpose");
     console.log(transpose);
 }
-let arr = 
-[[2, 3],[4, 5],[6, 7]];
+let arr = [[2, 3],[4, 5],[6, 7]];
 
 // transpose(arr,3,2);
 
@@ -69,6 +68,136 @@ function perfectNumber(number){
     return true;
 }
 
-console.log(perfectNumber(30));
+// console.log(perfectNumber(30));
+
+
+
+function checkStrongPass(str){
+    //Validation 
+    str=str.trim();
+    console.log(str);
+    const upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCase="abcdefghijklmnopqrstuvwxyz";
+    const numbers="0123456789";
+    const special="!@#$%&*";
+
+    let isUpperCase=false;
+    let isLowerCase=false;
+    let isNumber=false;
+    let isSpecial=false;
+
+    if(str.length<8){
+        return false ;
+    }
+    for(let i of str){
+        if(upperCase.includes(i)){
+            isUpperCase=true;
+        }else if(lowerCase.includes(i)){
+            isLowerCase=true;
+        }else if(numbers.includes(i)){
+            isNumber=true;
+        }else if(special.includes(i)){
+            isSpecial=true;
+        }
+    }
+    if(isUpperCase&&isLowerCase&&isNumber&&isSpecial){
+        return true;
+    }
+    return false;
+}
+// console.log(checkStrongPass("Saru@786khan   "));
+
+
+function matrixMultiplication(A,B){
+    let Arow=A.length;
+    let Acol=A[0].length;
+    let Brow=B.length;
+    let Bcol=B[0].length;
+    // Final matrix is of Arow*Bcol
+    if(Acol!=Brow){
+        throw new Error("Inputs are wrong !");
+    }
+    let ans=[];
+    for(let i=0;i<Arow;i++){
+        ans[i] = []; // Initialize row
+        for(let j=0;j<Bcol;j++){
+            ans[i][j]=0;
+            for(let k=0;k<Acol;k++){
+                ans[i][j]+= A[i][k]*B[k][j];
+            }
+        }
+    }
+    return ans;
+   
+}
+
+const A = [
+    [1, 2, 3],
+    [4, 5, 6]
+];
+
+const B = [
+    [7, 8],
+    [9, 10],
+    [11, 12]
+];
+
+// console.log(matrixMultiplication(A, B));
+
+// Tip Calculation 
+function calculateTip(amt,arr){
+    let tipArr=[];
+    for( i of arr){
+        let sum=(i*amt)/100;
+        sum=sum.toFixed(2)
+        // tipArr.push(sum);
+        const formatter=Intl.NumberFormat("en-IN",{
+            style:"currency",
+            currency:"INR"
+        })
+        tipArr.push(formatter.format(sum));
+    }
+    console.log(tipArr);
+}
+// calculateTip(100,[15,30,20]);
+
+// Learnt about Formatter !
+
+
+// Program to find Palindromic Substrings
+
+// let s="hellex";
+// let y=s.split('').reverse().join('');
+// console.log(s,y);
+
+function checkPali(str){
+    str=str.trim();
+    let strArr=str.trim().toLowerCase().split('');
+    strArr=strArr.reverse();
+    let revStr=strArr.join('');
+    if(revStr==str.toLowerCase()){
+        return true;
+    }
+    return false;
+}
+// console.log(checkPali("Madam"));
+
+function findPaliSubString(str){
+    // mamaradarmamaradar
+    let n=str.length;
+    let arr=[];
+    for(let i=0;i<n;i++){
+        for(let j=i+1;j<=n;j++){
+            let substr=str.slice(i,j);
+            if(substr.length>1&&checkPali(substr)){
+                // check condition
+                if(!arr.includes(substr))
+                arr.push(substr);
+            }
+        }
+    }
+    console.log(arr);
+}
+// findPaliSubString("mamaradarmamaradar");
 
 
